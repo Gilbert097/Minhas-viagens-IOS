d1c8478394d2eaeea35d33c7e2e363075a9e6591
Gilberto Silva <gilberto.silva@sigga.com>
2021-02-15 15:23:41 -0300

    Criando layout do app.

---
+++ Minhas viagens/Minhas viagens/controller/TravelMapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Minhas viagens
+//
+//  Created by Gilberto Silva on 15/02/21.
+//
+
+import UIKit
+
+class TravelMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
