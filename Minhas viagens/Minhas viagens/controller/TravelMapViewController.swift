c68fdf84f6ff1809bdccf3ef84bc5ab5af31dbfd
Gilberto Silva <gilberto.silva@sigga.com>
2021-02-15 18:47:01 -0300

    Criando ponto no mapa e recuperando o endereço.

--- Minhas viagens/Minhas viagens/controller/TravelMapViewController.swift
+++ Minhas viagens/Minhas viagens/controller/TravelMapViewController.swift
@@ -12,15 +12,95 @@ class TravelMapViewController: UIViewController, MKMapViewDelegate, CLLocationMa
     
     @IBOutlet weak var mapView: MKMapView!
     private var locationManager = CLLocationManager()
+    private let geocoder = CLGeocoder()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         initLocationManager()
+        registerMapLongPressGesture()
     }
+    
     private func initLocationManager() {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
+    }
+    
+    private func registerMapLongPressGesture() {
+        let mapLongPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(self.onMapLongPress(gesture:)))
+        mapView.addGestureRecognizer(mapLongPressGesture)
+    }
+    
+    @objc func onMapLongPress(gesture: UIGestureRecognizer){
+        switch gesture.state {
+        case .possible:
+            print("gesture possible")
+        case .began:
+            print("gesture began")
+            onMapGestureBegan(gesture)
+        case .changed:
+            print("gesture changed")
+        case .ended:
+            print("gesture ended")
+        case .cancelled:
+            print("gesture cancelled")
+        case .failed:
+            print("gesture failed")
+        @unknown default:
+            print("Error gesture state nil")
+        }
+    }
+    
+    private func onMapGestureBegan(_ gesture: UIGestureRecognizer) {
+        let pointSelected = gesture.location(in: self.mapView)
+        let coordinate = self.mapView.convert(pointSelected ,toCoordinateFrom: self.mapView)
+        let location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        findAddressByLocation(location){ address in
+            self.applyPointAnnotationByAddress(coordinate, address)
+        }
+    }
+    
+    private func applyPointAnnotationByAddress(_ location: CLLocationCoordinate2D, _ address: Address?) {
+        let pointAnnotation = MKPointAnnotation()
+        pointAnnotation.coordinate.latitude = location.latitude
+        pointAnnotation.coordinate.longitude = location.longitude
+        if let addressReceived = address {
+            if(!addressReceived.name.isEmpty){
+                pointAnnotation.title = addressReceived.name
+                pointAnnotation.subtitle = addressReceived.getShortAddress()
+            }else{
+                pointAnnotation.title = addressReceived.getShortAddress()
+                pointAnnotation.subtitle = ""
+            }
+        }
+        
+        self.mapView.addAnnotation(pointAnnotation)
+    }
+    
+    private func findAddressByLocation(_ location: CLLocation, completion: @escaping (Address?)-> (Void)) {
+        geocoder.reverseGeocodeLocation(location) { (placemarks, error) in
+            if error == nil {
+                
+                if let placemarksRecived = placemarks,
+                   let placemarkFirst = placemarksRecived.first {
+                    let address =  Address(name: placemarkFirst.name ?? "",
+                                           thoroughfare: placemarkFirst.thoroughfare ?? "",
+                                           subThoroughfare: placemarkFirst.subThoroughfare ?? "",
+                                           locality: placemarkFirst.locality ?? "",
+                                           subLocality: placemarkFirst.subLocality ?? "",
+                                           postalCode: placemarkFirst.postalCode ?? "",
+                                           country: placemarkFirst.country ?? "",
+                                           administrativeArea: placemarkFirst.administrativeArea ?? "",
+                                           subAdministrativeArea: placemarkFirst.subAdministrativeArea ?? "")
+                    completion(address)
+                }else{
+                    completion(nil)
+                }
+            }else{
+                completion(nil)
+                print("Error: \(String(describing: error))")
+            }
+        }
     }
     
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
@@ -71,7 +151,7 @@ class TravelMapViewController: UIViewController, MKMapViewDelegate, CLLocationMa
         alertController.addAction(cancelAction)
     }
     
-
+    
     
     
 }
---
+++ Minhas viagens/Minhas viagens/model/Address.swift
@@ -0,0 +1,60 @@
+//
+//  LocationAddress.swift
+//  Minhas viagens
+//
+//  Created by Gilberto Silva on 15/02/21.
+//
+
+import Foundation
+
+class Address {
+    var name = ""
+    var thoroughfare =  ""
+    var subThoroughfare = ""
+    var locality = ""
+    var subLocality =  ""
+    var postalCode =  ""
+    var country =  ""
+    var administrativeArea =  ""
+    var subAdministrativeArea =  ""
+    
+    init(name:String = "",
+         thoroughfare:String = "",
+         subThoroughfare:String = "",
+         locality:String = "",
+         subLocality :String = "",
+         postalCode:String = "",
+         country:String = "",
+         administrativeArea:String = "",
+         subAdministrativeArea:String = "") {
+        self.name = name
+        self.thoroughfare = thoroughfare
+        self.subThoroughfare = subThoroughfare
+        self.locality = locality
+        self.subLocality = subLocality
+        self.postalCode = postalCode
+        self.country = country
+        self.administrativeArea = administrativeArea
+        self.subAdministrativeArea = subAdministrativeArea
+        
+        if name == "\(thoroughfare), \(subThoroughfare)"{
+            self.name = ""
+        }
+    }
+    
+    func getShortAddress() -> String {
+        "\(thoroughfare) - \(subThoroughfare) / \(locality) / \(country)"
+    }
+    
+    func toString()->String{
+            "\n name: \(name)" +
+            "\n thoroughfare: \(thoroughfare)" +
+            "\n subThoroughfare: \(subThoroughfare)" +
+            "\n locality: \(locality)" +
+            "\n subLocality: \(subLocality)" +
+            "\n postalCode: \(postalCode)" +
+            "\n country: \(country)" +
+            "\n administrativeArea: \(administrativeArea)" +
+            "\n subAdministrativeArea: \(subAdministrativeArea)"
+    }
+}
