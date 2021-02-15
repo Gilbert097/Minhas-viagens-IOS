2571b213c3e307458b77539122d4587fa3872a15
Gilberto Silva <gilberto.silva@sigga.com>
2021-02-15 15:38:05 -0300

    Importado framework CoreLocation e implementado permissão de acesso a localização do usuário.

--- Minhas viagens/Minhas viagens/controller/TravelMapViewController.swift
+++ Minhas viagens/Minhas viagens/controller/TravelMapViewController.swift
@@ -6,14 +6,73 @@
 //
 
 import UIKit
+import MapKit
 
-class TravelMapViewController: UIViewController {
-
+class TravelMapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate  {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    private var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initLocationManager()
     }
+    private func initLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        
+        let status =  manager.authorizationStatus
+        switch status {
+        case .notDetermined:
+            print("Status: notDetermined")
+        case .restricted:
+            print("Status: restricted")
+        case .denied:
+            print("Status: denied")
+            showAlertRequestPermissionLocation()
+        case .authorizedAlways:
+            print("Status: authorizedAlways")
+        case .authorizedWhenInUse:
+            print("Status: authorizedWhenInUse")
+            locationManager.startUpdatingLocation()
+        @unknown default:
+            print("Status: nil")
+        }
+    }
+    
+    
+    private func showAlertRequestPermissionLocation() {
+        let alertController = UIAlertController(title: "Permissão de localização",
+                                                message: "Necessário permissão para à sua localização!",
+                                                preferredStyle: .actionSheet)
+        addAlertActions(alertController)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    private func addAlertActions(_ alertController: UIAlertController) {
+        let configurationAction = UIAlertAction(title: "Abrir configurações",
+                                                style: .default) { (action) in
+            
+            if let bundleId = Bundle.main.bundleIdentifier,
+               let url = URL(string: "\(UIApplication.openSettingsURLString)&path=LOCATION/\(bundleId)"),
+               UIApplication.shared.canOpenURL(url)
+            {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .default, handler: nil)
+        
+        alertController.addAction(configurationAction)
+        alertController.addAction(cancelAction)
+    }
+    
 
-
+    
+    
 }
 
